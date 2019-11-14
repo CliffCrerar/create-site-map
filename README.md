b00ca4071407cbef8678946b8e66a94a883540fc
CliffCrerar <cliff.crerar@gmail.com>
2019-11-14 15:22:43 +0200

    What?

--- README.md
+++ README.md
@@ -6,8 +6,21 @@ copy this into the browser console command line:
 
 ```js
  var links = []; document.querySelectorAll('a').forEach(l=>links.push({loc: l.href, lastmod: new Date().toISOString()}));JSON.stringify(links);
-
 ```
 copy the output of the console into a `foo.json` file.
 
-run the command `node app foo.json`
+run the command: 
+
+```bash 
+node app [path to json file]/foo.jso
+```
+
+NOTE: Easiest to create the file in the `root` directory of this project and simply run:
+
+```bash
+node app foo.json
+```
+### VERBOSELY
+
+To see the process console logs add `verbose` to the end of the command.
+
--- app.js
+++ app.js
@@ -16,26 +16,34 @@
 			</url>
 	</urlset>
   */
-process.argv
-console.log('process.argv: ', process.argv);
+process.argv[2];
+const verbose = process.argv[3] === 'verbose';
 const priority = 0.2
 const xml = require('jsontoxml').json_to_xml;
-console.log('xml: ', xml);
-const links = require('./site-map-links.json').map(({ loc, lastmod }) => {
-    const url = { loc, lastmod, priority }
-    return { url };
-});
-console.log('links: ', links);
-console.log(xml(links));
+let links;
+try {
+    links = require(require('path').resolve(process.argv[2].split('.')[0] + '.json'))
+        .map(({ loc, lastmod }) => {
+            const url = { loc, lastmod, priority }
+            return { url };
+        });
+    verbose && console.log('links: ', links);
+    if (links.length === 0) throw new Error('The file is empty');
+    createSiteMap(links, () => console.log('sitemap file created in project root'));;
+} catch (err) {
+    console.error('ERROR', err.message);
+    verbose && console.error(err);
+}
 
-const siteMapXmlString = `
-<urlset 
-xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
-xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
-xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-${xml(links)}
-</urlset>
-`
-console.log('siteMapXmlString: ', siteMapXmlString);
-
-require('fs').writeFileSync('./sitemap.xml', siteMapXmlString, 'utf8');
+function createSiteMap(linksArray, cb) {
+    const siteMapXmlString = `
+	<urlset 
+	xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+	xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+	${xml(linksArray)}
+	</urlset>`;
+    verbose && console.log('siteMapXmlString: ', siteMapXmlString);
+    require('fs').writeFileSync('./sitemap.xml', siteMapXmlString, 'utf8');
+    cb()
+}
